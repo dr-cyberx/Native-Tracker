31ed1ca720319180393f14c6c65b49bf99a13706
Cd-Vishal007 <vishal.codedrill@gmail.com>
2021-08-20 18:14:43 +0530

    created an jwt setup

---
+++ backend/src/middlewares/requireAuth.js
@@ -0,0 +1,25 @@
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+import User from "../models/Users";
+
+export default (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization) {
+    return res.status(401).send({ errMessage: "You must be logged in" });
+  }
+
+  const token = authorization.replace("Bearer ", "");
+  jwt.verify(token, "MY_SECRET_KEY", async (err, payload) => {
+    if (err) {
+      res.status(401).send({ errMessage: "You must be logged in" });
+    }
+
+    const { userId } = payload;
+    const user = await User.findById(userId);
+
+    req.user = user;
+
+    next();
+  });
+};
