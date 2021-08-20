b723d7dcebec387ebc40e2e69e52e338daefadcf
Cd-Vishal007 <vishal.codedrill@gmail.com>
2021-08-20 17:56:54 +0530

    introducing jwt token

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
 import User from "../models/Users";
 
 const authRoute = Router();
@@ -7,9 +8,15 @@ const authRoute = Router();
 authRoute.post("/signup", async (req, res) => {
   const { email, password } = req.body;
 
-  const newUser = new User({ email, password });
-  await newUser.save();
-  res.send(req.body);
+  try {
+    const newUser = new User({ email, password });
+    await newUser.save();
+
+    const token = jwt.sign({userId: newUser._id}, 'MY_SECRET_KEY');
+    res.send({token});
+  } catch (err) {
+    return res.status(422).send(err.message);
+  }
 });
 
 export default authRoute;
