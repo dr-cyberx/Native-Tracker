f2f36bc7f61263dbb85e25d1b7ca264d824c9044
Cd-Vishal007 <vishal.codedrill@gmail.com>
2021-08-23 10:45:29 +0530

    added basic page setup

---
+++ client/Src/Screens/SigninScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function SigninScreen() {
+  return (
+    <View>
+      <Text>Sign In Screen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
