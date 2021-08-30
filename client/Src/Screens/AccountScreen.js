c217672b05e7d6bb60f448b429146ab44a7567c5
Cd-Vishal007 <vishal.codedrill@gmail.com>
2021-08-30 18:31:19 +0530

    done with the authentiation part and the sign out feature of account screen

--- client/Src/Screens/AccountScreen.js
+++ client/Src/Screens/AccountScreen.js
@@ -1,14 +1,15 @@
 import React, { useContext } from "react";
 import { StyleSheet } from "react-native";
 import AuthContext from "../context/AuthContext";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { Text, Button } from "react-native-elements";
 import Spacer from "../components/Spacer";
 
 const AccountScreen = ({ navigation }) => {
   const { signout } = useContext(AuthContext);
   return (
-    <>
-      <Text h4 style={{ fontWeight: "bold" }}>
+    <SafeAreaView style={{marginTop: 25, paddingHorizontal: 25}} edges={["top", "right", "left", "bottom"]}>
+      <Text h2  style={{ fontWeight: "bold" }}>
         Account Screen
       </Text>
       <Spacer />
@@ -20,7 +21,7 @@ const AccountScreen = ({ navigation }) => {
         }}
         title="Sign Out"
       />
-    </>
+    </SafeAreaView>
   );
 };
 
