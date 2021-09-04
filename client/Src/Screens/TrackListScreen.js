1fd2e0d01b03b2bb008d7ae5a34e750b348da0db
Cd-Vishal007 <vishal.codedrill@gmail.com>
2021-09-04 18:05:23 +0530

    adding list

--- client/Src/Screens/TrackListScreen.js
+++ client/Src/Screens/TrackListScreen.js
@@ -1,41 +1,49 @@
 import React, { useContext } from "react";
 import { FlatList } from "react-native";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import { ListItem } from "react-native-elements";
+import { StyleSheet, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { ListItem, Text } from "react-native-elements";
 import TrackContext from "../context/TrackContext";
 
 const TrackListScreen = ({ navigation }) => {
   const { state, fetchTrack } = useContext(TrackContext);
 
-  console.log(state);
-
   React.useEffect(() => {
     const unsubscribe = navigation.addListener("focus", () => {
       fetchTrack();
+      console.log({ ...state });
     });
 
     return unsubscribe;
   }, []);
 
   return (
-    <View>
+    <SafeAreaView
+      style={{ marginTop: 25, paddingHorizontal: 25 }}
+      edges={["top", "right", "left", "bottom"]}
+    >
+      <Text style={{ textAlign: "center" }} h1>
+        Tracks Lists
+      </Text>
       <FlatList
-        data={state}
+        style={{ marginTop: 20 }}
+        data={state.track}
         keyExtractor={(item) => {
-          item._id;
+          return item._id;
         }}
         renderItem={({ item }) => (
-          <TouchableOpacity>
+          <TouchableOpacity style={{ marginTop: 10 }}>
             <ListItem key={item._id} bottomDivider>
               <ListItem.Content>
-                <ListItem.Title>{item.title}</ListItem.Title>
+                {console.log("item => ", item)}
+                <ListItem.Title>{item.name}</ListItem.Title>
               </ListItem.Content>
               <ListItem.Chevron />
             </ListItem>
           </TouchableOpacity>
         )}
       />
-    </View>
+    </SafeAreaView>
   );
 };
 
