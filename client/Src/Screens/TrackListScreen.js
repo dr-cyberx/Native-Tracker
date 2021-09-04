01dd7a6f0d29b28a7c7649e890bae26bce14eba9
Cd-Vishal007 <vishal.codedrill@gmail.com>
2021-09-04 17:48:40 +0530

    changes

--- client/Src/Screens/TrackListScreen.js
+++ client/Src/Screens/TrackListScreen.js
@@ -1,18 +1,40 @@
-import React from "react";
+import React, { useContext } from "react";
+import { FlatList } from "react-native";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { ListItem } from "react-native-elements";
+import TrackContext from "../context/TrackContext";
 
 const TrackListScreen = ({ navigation }) => {
+  const { state, fetchTrack } = useContext(TrackContext);
+
+  console.log(state);
+
+  React.useEffect(() => {
+    const unsubscribe = navigation.addListener("focus", () => {
+      fetchTrack();
+    });
+
+    return unsubscribe;
+  }, []);
+
   return (
     <View>
-      <Text>Track List Screen</Text>
-
-      <TouchableOpacity
-        onPress={() => {
-          navigation.navigate("TrackCreate");
+      <FlatList
+        data={state}
+        keyExtractor={(item) => {
+          item._id;
         }}
-      >
-        <Text style={{ fontSize: 25, color: "lightblue" }}>Go to Track Create Screen </Text>
-      </TouchableOpacity>
+        renderItem={({ item }) => (
+          <TouchableOpacity>
+            <ListItem key={item._id} bottomDivider>
+              <ListItem.Content>
+                <ListItem.Title>{item.title}</ListItem.Title>
+              </ListItem.Content>
+              <ListItem.Chevron />
+            </ListItem>
+          </TouchableOpacity>
+        )}
+      />
     </View>
   );
 };
--- client/Src/context/TrackContext.js
+++ client/Src/context/TrackContext.js
@@ -1,16 +1,33 @@
-import React, { createContext } from "react";
+import React, { createContext, useReducer } from "react";
 import tracker from "../api/tracker";
 
 const TrackContext = createContext();
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "fetch_tracks":
+      return action.payload;
+
+    default:
+      break;
+  }
+};
+
 export const TrackProvider = ({ children }) => {
-  const fetchTrack = () => {};
+  const [state, dispatch] = useReducer(reducer);
+
+  const fetchTrack = async () => {
+    const response = await tracker.get("/track");
+    // console.log("response data => ", response.data)
+    dispatch({ type: "fetch_tracks", payload: response.data });
+  };
+
   const createTrack = async (name, locations) => {
-    console.log(name, locations.length);
-    await tracker.post('/track', {name , locations})
+    // console.log(name, locations.length);
+    await tracker.post("/track", { name, locations });
   };
   return (
-    <TrackContext.Provider value={{ fetchTrack, createTrack }}>
+    <TrackContext.Provider value={{ fetchTrack, createTrack, state }}>
       {children}
     </TrackContext.Provider>
   );
