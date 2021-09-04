e9eb0b7786a07c51cb23a9dde6be4286ed9b52f0
Cd-Vishal007 <vishal.codedrill@gmail.com>
2021-09-04 16:19:40 +0530

    changes

--- client/Src/context/TrackContext.js
+++ client/Src/context/TrackContext.js
@@ -1,15 +1,18 @@
-import React, {createContext} from 'react'
-import TrackCreateScreen from '../Screens/TrackCreateScreen';
+import React, { createContext } from "react";
+import TrackCreateScreen from "../Screens/TrackCreateScreen";
 
 const TrackContext = createContext();
 
-
-export const TrackProvider = ({children}) => {
+export const TrackProvider = ({ children }) => {
+  const fetchTrack = () => {};
+  const createTrack = (name, locations) => {
+    console.log(name, locations.length);
+  };
   return (
-    <TrackContext.Provider value={{}}>
-    {children}
+    <TrackContext.Provider value={{ fetchTrack, createTrack }}>
+      {children}
     </TrackContext.Provider>
-  )
-}
+  );
+};
 
 export default TrackContext;
